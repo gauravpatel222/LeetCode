f72f890673545e4e5f673ae27b05f7af6b0f260e
gauravpatel222 <108782700+gauravpatel222@users.noreply.github.com>
2023-08-15 13:52:15 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -58,32 +58,35 @@ class Main {
 /*Complete the function below*/
 
 
-class Solution {
-    static int[] topoSort(int n, ArrayList<ArrayList<Integer>> nums) {
-        int[] vis = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == 0) {
-                rec(nums, stack, vis, i);
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int n, ArrayList<ArrayList<Integer>> nums) 
+    {
+        int[] vis=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                rec(nums,stack,vis,i);
             }
         }
-        
-        int[] ans = new int[n];
-        int i = 0;
-        while (!stack.isEmpty()) {
-            ans[i++] = stack.pop();
+        int[] ans=new int[n];
+        int i=0;
+        while(!stack.isEmpty()){
+            ans[i++]=stack.pop();
         }
         return ans;
     }
-    
-    public static void rec(ArrayList<ArrayList<Integer>> nums, Stack<Integer> stack, int[] vis, int i) {
-        vis[i] = 1;
-        for (int neighbor : nums.get(i)) {
-            if (vis[neighbor] == 0) {
-                rec(nums, stack, vis, neighbor);
+    public static void rec(ArrayList<ArrayList<Integer>> nums,Stack<Integer> stack,int[] vis,int i){
+        vis[i]=1;
+        for(int index=0;index<nums.get(i).size();index++){
+            if(vis[nums.get(i).get(index)]==0){
+                rec(nums,stack,vis,nums.get(i).get(index));
+                
             }
+           
         }
-        stack.push(i);
+         stack.push(i);
     }
 }
+
